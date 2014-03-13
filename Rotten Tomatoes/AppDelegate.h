da2d6f30fe0eedcb08081130d74aabaa0d37c45f
Ben Chang <mrclam@gmail.com>
2014-03-12 22:31:55 -0700

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Benjamin Chang on 3/12/14.
+//  Copyright (c) 2014 Benjamin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
